42151ca590ac728a1e89886f10f7e684b67a9239
fboris <fantasyboris@gmail.com>
2014-01-30 22:53:34 +0800

    Finish USART Example continuously transimit data

---
+++ USART/main.c
@@ -0,0 +1,103 @@
+#include "stm32f10x.h"
+#include "stm32f10x_conf.h"
+
+void delay(uint32_t delay_count)
+{
+	while (delay_count) delay_count--;
+}
+void init_led()
+{
+	GPIO_InitTypeDef GPIO_InitStructure;
+	/* GPIOA Periph clock enable */
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA, ENABLE);
+
+	/* Configure PA0 and PA1 in output pushpull mode */
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_0 | GPIO_Pin_1;
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
+	GPIO_Init(GPIOA, &GPIO_InitStructure);
+}
+void gpio_toogle(GPIO_TypeDef *GPIOx, uint16_t GPIO_Pin)
+{
+	GPIOx->ODR ^= GPIO_Pin;
+}
+
+void init_usart1()
+{
+
+	USART_InitTypeDef USART_InitStructure;
+	GPIO_InitTypeDef GPIO_InitStructure;
+
+	/* Enable peripheral clocks. */
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA | RCC_APB2Periph_AFIO, ENABLE);
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_USART1, ENABLE);
+
+	/* Configure USART1 Rx pin as floating input. */
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_10;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IN_FLOATING;
+	GPIO_Init(GPIOA, &GPIO_InitStructure);
+
+	/* Configure USART1 Tx as alternate function push-pull. */
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_9;
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_PP;
+	GPIO_Init(GPIOA, &GPIO_InitStructure);
+
+	/* Configure the USART2 */
+	USART_InitStructure.USART_BaudRate = 9600;
+	USART_InitStructure.USART_WordLength = USART_WordLength_8b;
+	USART_InitStructure.USART_StopBits = USART_StopBits_1;
+	USART_InitStructure.USART_Parity = USART_Parity_No;
+	USART_InitStructure.USART_HardwareFlowControl = USART_HardwareFlowControl_None;
+	USART_InitStructure.USART_Mode = USART_Mode_Rx | USART_Mode_Tx;
+	USART_Init(USART1, &USART_InitStructure);
+	USART_Cmd(USART1, ENABLE);
+
+	NVIC_InitTypeDef NVIC_InitStructure;
+
+	/* Enable transmit and receive interrupts for the USART2. */
+	USART_ITConfig(USART1, USART_IT_TXE, DISABLE);
+	USART_ITConfig(USART1, USART_IT_RXNE, ENABLE);
+
+	/* Enable the USART1 IRQ in the NVIC module (so that the USART1 interrupt
+	 * handler is enabled). */
+	NVIC_InitStructure.NVIC_IRQChannel = USART1_IRQn;
+	NVIC_InitStructure.NVIC_IRQChannelSubPriority = 0;
+	NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
+	NVIC_Init(&NVIC_InitStructure);
+
+	/* Enable the RS232 port. */
+	USART_Cmd(USART1, ENABLE);
+
+}
+void send_byte(uint8_t b)
+{
+	/* Send one byte */
+	USART_SendData(USART1, b);
+
+	/* Loop until USART2 DR register is empty */
+	while (USART_GetFlagStatus(USART1, USART_FLAG_TXE) == RESET);
+}
+void send_string(uint8_t *str)
+{
+	while ((*str) != '\0') {
+
+		send_byte(*str);
+		str++;
+	}
+}
+int main(void)
+{
+	init_led();
+	init_usart1();
+	char *str = "test usart1!\r\n";
+
+	while (1) {
+		send_string(str);
+		gpio_toogle(GPIOA, GPIO_Pin_0);
+		gpio_toogle(GPIOA, GPIO_Pin_1);
+		delay(500000);
+
+	}
+}
+
