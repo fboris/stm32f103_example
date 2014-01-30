4a24e24732cfc6759d477c4fb162cbfa60626fa2
fboris <fantasyboris@gmail.com>
2014-01-30 23:46:05 +0800

    Add 'echo' demo in USART directory when user type some message, the MCU will transmit this message to terminal

---
+++ USART/echo/main.c
@@ -0,0 +1,59 @@
+#include "stm32f10x.h"
+#include "stm32f10x_conf.h"
+
+void init_usart1()
+{
+
+	USART_InitTypeDef USART_InitStructure;
+	GPIO_InitTypeDef GPIO_InitStructure;
+
+	/* Enable peripheral clocks. */
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA | RCC_APB2Periph_AFIO, ENABLE);
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_USART1, ENABLE);
+
+	/* Configure USART1 Rx pin as floating input. */
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_10;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IN_FLOATING;
+	GPIO_Init(GPIOA, &GPIO_InitStructure);
+
+	/* Configure USART1 Tx as alternate function push-pull. */
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_9;
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_PP;
+	GPIO_Init(GPIOA, &GPIO_InitStructure);
+
+	/* Configure the USART2 */
+	USART_InitStructure.USART_BaudRate = 9600;
+	USART_InitStructure.USART_WordLength = USART_WordLength_8b;
+	USART_InitStructure.USART_StopBits = USART_StopBits_1;
+	USART_InitStructure.USART_Parity = USART_Parity_No;
+	USART_InitStructure.USART_HardwareFlowControl = USART_HardwareFlowControl_None;
+	USART_InitStructure.USART_Mode = USART_Mode_Rx | USART_Mode_Tx;
+	USART_Init(USART1, &USART_InitStructure);
+	USART_Cmd(USART1, ENABLE);
+
+
+
+}
+void send_byte(uint8_t b)
+{
+	/* Send one byte */
+	USART_SendData(USART1, b);
+
+	/* Loop until USART2 DR register is empty */
+	while (USART_GetFlagStatus(USART1, USART_FLAG_TXE) == RESET);
+}
+
+int main(void)
+{
+	init_usart1();
+	char b;
+	while (1) {
+
+        	while(USART_GetFlagStatus(USART1, USART_FLAG_RXNE) == RESET);
+        	b =  (USART_ReceiveData(USART1) & 0x7F);
+        	send_byte(b);
+		
+	}
+}
+
