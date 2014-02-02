c40a9a9d0c34aa80dcb84daa024d6469f79f841b
fboris <fantasyboris@gmail.com>
2014-02-03 01:08:51 +0800

    Move USART functions to single file and fix some warning

--- USART/printf/usart.h
+++ USART/printf/usart.h
@@ -1,4 +1,6 @@
 #ifndef __USART_H
+
+
 typedef struct {
 	char(*getch)();		   //If declare as getc will cause naming conflict
 	void (*putch)(char str); 	  //If declare as putc will cause naming conflict
@@ -6,4 +8,8 @@ typedef struct {
 
 extern serial_ops serial;
 int printf(const char *format, ...);
+int puts(const char* msg);
+char *utoa(unsigned int num, char *dst, unsigned int base);
+char *itoa(int num, char *dst, int base);
+void init_usart1();
 #endif
