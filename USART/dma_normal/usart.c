cedf799ad092d16bb1ea6a536be72ba42ec56376
fboris <fantasyboris@gmail.com>
2014-04-05 18:15:07 +0800

    Add dma_normal to support dma normal mode In normal mode, DMA will only transmit data once.

---
+++ USART/dma_normal/usart.c
@@ -0,0 +1,112 @@
+#include "stm32f10x.h"
+#include "stm32f10x_conf.h"
+#include "usart.h"
+#include <unistd.h>
+#include <stdarg.h>
+#include <string.h>
+#include <ctype.h>
+uint8_t TxBuffer[] = "AAAAA\r\n";
+
+void init_usart1()
+{
+
+	USART_InitTypeDef USART_InitStructure;
+	GPIO_InitTypeDef GPIO_InitStructure;
+
+	/* Enable peripheral clocks. */
+	RCC_AHBPeriphClockCmd(RCC_AHBPeriph_DMA1, ENABLE);
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA | RCC_APB2Periph_AFIO, ENABLE);
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_USART1, ENABLE);
+
+	/* Configure USART1 Rx pin as floating input. */
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_10;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IN_FLOATING;
+	GPIO_Init(GPIOA, &GPIO_InitStructure);
+
+	/* Configure USART1 Tx as alternate function push-pull. */
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_9;
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_PP;
+	GPIO_Init(GPIOA, &GPIO_InitStructure);
+
+	/* Configure the USART2 */
+	USART_InitStructure.USART_BaudRate = 230400;
+	USART_InitStructure.USART_WordLength = USART_WordLength_8b;
+	USART_InitStructure.USART_StopBits = USART_StopBits_1;
+	USART_InitStructure.USART_Parity = USART_Parity_No;
+	USART_InitStructure.USART_HardwareFlowControl = USART_HardwareFlowControl_None;
+	USART_InitStructure.USART_Mode = USART_Mode_Tx;
+	USART_Init(USART1, &USART_InitStructure);
+
+
+	NVIC_InitTypeDef NVIC_InitStructure;
+
+	/* Enable transmit and receive interrupts for the USART2. */
+	USART_ITConfig(USART1, USART_IT_TXE, DISABLE);
+	USART_ITConfig(USART1, USART_IT_RXNE, DISABLE);
+
+}
+void usart_dma_send(uint8_t *s){
+
+	DMA_InitTypeDef DMA_InitStructure;
+	/* USARTy_Tx_DMA_Channel (triggered by USARTy Tx event) Config */
+	DMA_DeInit(DMA1_Channel4);
+	DMA_InitStructure.DMA_PeripheralBaseAddr = (uint32_t)&USART1->DR;
+	DMA_InitStructure.DMA_MemoryBaseAddr = (uint32_t)s;
+	DMA_InitStructure.DMA_DIR = DMA_DIR_PeripheralDST;
+	DMA_InitStructure.DMA_BufferSize = strlen((const char *)s);
+	DMA_InitStructure.DMA_PeripheralInc = DMA_PeripheralInc_Disable;
+	DMA_InitStructure.DMA_MemoryInc = DMA_MemoryInc_Enable;
+	DMA_InitStructure.DMA_PeripheralDataSize = DMA_PeripheralDataSize_Byte;
+	DMA_InitStructure.DMA_MemoryDataSize = DMA_MemoryDataSize_Byte;
+	DMA_InitStructure.DMA_Mode = DMA_Mode_Normal;
+	DMA_InitStructure.DMA_Priority = DMA_Priority_VeryHigh;
+	DMA_InitStructure.DMA_M2M = DMA_M2M_Disable;
+	DMA_Init(DMA1_Channel4, &DMA_InitStructure);
+	
+
+ 	USART_DMACmd(USART1, USART_DMAReq_Tx, ENABLE);
+ 	DMA_Cmd(DMA1_Channel4, ENABLE);
+	/* Enable the RS232 port. */
+	USART_Cmd(USART1, ENABLE);
+	while (DMA_GetFlagStatus(DMA1_FLAG_TC4) == RESET);
+	while(USART_GetFlagStatus(USART1, USART_FLAG_TC) == RESET);
+
+
+}
+void retarget_init()
+{
+  // Initialize UART
+}
+
+int _write (int fd, char *ptr, int len)
+{
+  /* Write "len" of char from "ptr" to file id "fd"
+   * Return number of char written.
+   * Need implementing with UART here. */
+  
+  for ( int i = 0; i<len ;i++)
+  {
+    send_byte(*ptr);
+    ptr++;
+  }
+  return len;
+}
+
+int _read (int fd, char *ptr, int len)
+{
+  /* Read "len" of char to "ptr" from file id "fd"
+   * Return number of char read.
+   * Need implementing with UART here. */
+  return len;
+}
+
+void _ttywrch(int ch) {
+  /* Write one char "ch" to the default console
+   * Need implementing with UART here. */
+  /* Send one byte */
+  USART_SendData(USART1, ch);
+
+  /* Loop until USART2 DR register is empty */
+  while (USART_GetFlagStatus(USART1, USART_FLAG_TXE) == RESET);
+}
