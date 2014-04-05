7ea6e69fca58ed3d60881be8c20966b8d312f994
fboris <fantasyboris@gmail.com>
2014-04-05 17:36:42 +0800

    Add usart dma example This example uses cirular mode to transmit data.

---
+++ USART/dma/main.c
@@ -0,0 +1,54 @@
+#include "stm32f10x.h"
+#include "stm32f10x_conf.h"
+#include "usart.h"
+#include <stdio.h>
+extern uint8_t TxBuffer[32];
+extern DMA_InitTypeDef DMA_InitStructure;
+void delay(uint32_t delay_count)
+{
+	while (delay_count) delay_count--;
+}
+void init_led()
+{
+	GPIO_InitTypeDef GPIO_InitStructure;
+	/* GPIOA Periph clock enable */
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA, ENABLE);
+
+	/* Configure PA0 and PA1 in output pushpull mode */
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_0 | GPIO_Pin_1;
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
+	GPIO_Init(GPIOA, &GPIO_InitStructure);
+}
+void gpio_toggle(GPIO_TypeDef *GPIOx, uint16_t GPIO_Pin)
+{
+	GPIOx->ODR ^= GPIO_Pin;
+}
+
+void send_byte(uint8_t b)
+{
+	/* Send one byte */
+	USART_SendData(USART1, b);
+}
+void send_string(uint8_t *str)
+{
+	while ((*str) != '\0') {
+
+		send_byte(*str);
+		str++;
+	}
+}
+int main(void)
+{
+	init_led();
+	init_usart1();
+	int32_t counter = 0;
+	while(1){
+		counter++;
+		sprintf( TxBuffer, "%d\r\n",counter);
+		delay(20000);
+		
+	}
+
+}
+
---
+++ USART/dma/usart.h
@@ -0,0 +1,6 @@
+#ifndef __USART_H
+
+
+int _write (int fd, char *ptr, int len);
+void init_usart1();
+#endif
